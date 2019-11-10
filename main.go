b376a50aa69907c6a3c0d96728dbabf3370eef67
ivz hh <454153+ivzhh@users.noreply.github.com>
2019-11-10 09:41:36 -0700

    General testing settings

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"os"
 	"path"
+	"path/filepath"
+	"reflect"
 
 	"github.com/ivzhh/go-ast-transform/rewriter"
 
@@ -17,8 +19,30 @@ import (
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
+	pkgname := reflect.TypeOf(struct{}{}).PkgPath()
+
+	tmpdir := filepath.Join(os.TempDir(), pkgname)
+
+	if err := os.MkdirAll(tmpdir, 0644); err != nil {
+		log.Fatalf("create output dir fail: %s", err.Error())
+	}
+
+	inputfile := path.Join("testdata", "example1.go")
+
+	args := os.Args[1:]
+
+	if len(args) > 1 {
+		log.Fatalf("please run command: %s path_to_go_file.go", pkgname)
+	} else if len(args) == 1 {
+		inputfile = args[0]
+	}
+
+	if _, err := os.Stat(inputfile); os.IsNotExist(err) {
+		log.Fatalf("input go file doesn't exist: %s", inputfile)
+	}
+
 	fset := token.NewFileSet()
-	file, err := parser.ParseFile(fset, path.Join("testdata", "example1.go"), nil, parser.ParseComments)
+	file, err := parser.ParseFile(fset, inputfile, nil, parser.ParseComments)
 	if err != nil {
 		panic(err)
 	}
@@ -31,9 +55,13 @@ func main() {
 
 	var f *os.File
 
-	if f, err = os.Create("/tmp/dat2"); err != nil {
+	outputfile := filepath.Join(tmpdir, filepath.Base(inputfile))
+
+	if f, err = os.Create(outputfile); err != nil {
 		log.Fatalf("create file: %+v", err)
 	}
+
+	log.Printf("output to %s", outputfile)
 
 	ast.Fprint(bufio.NewWriter(f), fset, rewritten, nil)
 }
--- testdata/example1.go
+++ testdata/example1.go
@@ -2,7 +2,7 @@
 
 package main
 
-func foo(a int, b *int, c struct{ e, f int }) (g struct{ h, i float32 }) {
+func foo(a int, b *int, c struct{ e, f int }, d *struct{ e, f int }) (g struct{ h, i float32 }) {
 	g.h = float32(a + *b)
 	g.i = float32(c.e * c.f)
 
